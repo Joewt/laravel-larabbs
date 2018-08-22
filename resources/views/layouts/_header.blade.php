2da9f8556b6c838a95580cd773500b642eadc0dd
joe <joewt@163.com>
2018-08-22 11:57:22 +0800

    完成了页面基本布局

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-default navbar-static-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsing" data-toggle="collapse" data-target="#app-navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+
+            <a href="{{ url('/') }}" class="navbar-brand">
+                Larabbs
+            </a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="app-navbar-collapse">
+            <ul class="nav navbar-nav">
+
+            </ul>
+
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#">login</a></li>
+                <li><a href="#">register</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
