ea7aa783cd25928ec584c99ec44ae166edc86997
joe <joewt@163.com>
2018-08-25 12:31:41 +0800

    seo优化

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Topic;
+use App\Jobs\TranslateSlug;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -24,5 +25,15 @@ class TopicObserver
     {
         $topic->body = clean($topic->body, 'user_topic_body');
         $topic->excerpt = make_excerpt($topic->body);
+
+
+    }
+
+
+    public function saved(Topic $topic)
+    {
+        if(!$topic->slug){
+            dispatch(new TranslateSlug($topic));
+        }
     }
 }
