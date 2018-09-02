311e859a734dabd0c191bb6eecb8c35ce55ff2b8
joe <joewt@163.com>
2018-09-02 20:48:23 +0800

    接口限制

--- routes/api.php
+++ routes/api.php
@@ -18,9 +18,15 @@ $api->version('v1', [
     'namespace'=>'App\Http\Controllers\Api'
 ],function($api) {
 
-    $api->post('verificationCodes', 'VerificationCodesController@store')->name('api.verificationCodes.store');
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit' => config('api.rate_limits.sign.limit'),
+        'expires' => config('api.rate_limits.sign.expires'),
+    ], function($api){
+        $api->post('verificationCodes', 'VerificationCodesController@store')->name('api.verificationCodes.store');
 
-    // 用户注册
-    $api->post('users', 'UsersController@store')->name('api.users.store');
+        // 用户注册
+        $api->post('users', 'UsersController@store')->name('api.users.store');
+    });
 
 });
